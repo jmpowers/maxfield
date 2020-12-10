025edad9436fc41c2b131a7b82bdd987cc4f76b2
jmpowers <icyjava@gmail.com>
2020-12-10 11:31:11 -0800

    Megatally to statuses, rowbinding all floral traits

--- maxfield_data.R
+++ maxfield_data.R
@@ -3,11 +3,8 @@
 
 # TODO write metadata  to /metadata for the final data files
 # TODO add the following datasets:
-#       + census 2018 - loaded but needs to be cleaned up and merged with other years
-#       + phenology 2018, 2019 - loaded but need to be cleaned up
 #       + floral volatiles 2018, 2019
 #       + leaf physiology measures (LICOR) 2018, 2019, 2020
-#       + seeds 2019, 2020
 
 # setup -------------------------------------------------------------------
 
@@ -170,7 +167,7 @@ cen18 <- read_sheet(filter(datasheets, name=="2020 Maxfield Rosettes"), sheet="2
   mutate(plot = as.character(plot),
          plotid = paste0(plot, subplot),
          plant = as.character(plant),
-         id = paste(plotid,plant, sep="-"),
+         plantid = paste0(plotid,plant),
          year=factor(year(date)), round=1,
          notes =  fct_explicit_na(notes, "blank"),
          flowering = fct_explicit_na(flowering, "blank"),
@@ -183,14 +180,15 @@ cen18 <- read_sheet(filter(datasheets, name=="2020 Maxfield Rosettes"), sheet="2
 cen18r2 <- read_sheet(filter(datasheets, name=="2020 Maxfield Rosettes"), sheet="2018round2") %>% 
   mutate_at(vars(ends_with(c("longest","leaves"))), ~as.integer(as.character(.))) %>% 
   mutate(plant = as.character(plant),
-         id = paste(plotid,plant, sep="-"),
+         plantid = paste0(plotid,plant),
          plot = factor(str_sub(plotid,1,1)), subplot = factor(str_sub(plotid,2,2)),
-         year="2018",round=2,
+         year="2018",round=2,date=cen18$date[1]+months(2), #TODO find date of second 2018 census
          flowering = recode(notes, "flowering"="y",.missing="n",.default="n"),
          notes =  notes %>% fct_explicit_na("blank") %>% recode("flowering"="blank"),
          n_rosettes = as.integer(as.character(rosettes)),
          rosettes = recode(as.character(rosettes), .missing="blank",.default="one_or_more"),
-         status = recode(factor(paste(notes, rosettes, flowering)), !!!translate_nrf_18, .default="recheck") %>% fct_relevel(status_priority)) %>% 
+         status = recode(factor(paste(notes, rosettes, flowering)), !!!translate_nrf_18, .default="recheck") %>% 
+           fct_expand(status_priority) %>% fct_relevel(status_priority)) %>% 
   left_join(treatments) %>%
   mutate_if(is.character, as.factor)
 
@@ -198,6 +196,7 @@ cen19 <- read_sheet(filter(datasheets, name=="2020 Maxfield Rosettes"), sheet="2
   mutate(plot = as.character(plot),
          plotid = paste0(plot, subplot),
          plant = as.character(plant),
+         plantid = paste0(plotid,plant),
          year=factor(year(date)),
          notes = other_notes %>% str_match("tagnf|nf|dead|chewed|eaten")  %>% factor %>% 
            fct_explicit_na("blank") %>% recode(eaten="chewed"),
@@ -206,9 +205,7 @@ cen19 <- read_sheet(filter(datasheets, name=="2020 Maxfield Rosettes"), sheet="2
          rosettes = recode(as.character(rosettes), ID = "indist", `0` = "zero" , `NULL`="blank",.default="one_or_more", ),
          status = recode(paste(notes, rosettes, flowering), !!!translate_nrf_19, .default="recheck") %>% fct_relevel(status_priority),
          r1_longest	= ifelse(rosettes=="zero", NA, r1_longest),
-         r1_leaves = ifelse(rosettes=="zero", NA, r1_leaves),
-         id = na_if(id, "-")) %>% 
-  drop_na(id) %>%
+         r1_leaves = ifelse(rosettes=="zero", NA, r1_leaves)) %>% 
   left_join(treatments) %>%
   mutate_if(is.character, as.factor)
 
@@ -217,20 +214,56 @@ cen20 <- read_sheet(filter(datasheets, name=="2020 Maxfield Rosettes"), sheet="2
   mutate(plot = as.character(plot),
          plotid = paste0(plot, subplot),
          plant = as.character(plant),
+         plantid = paste0(plotid,plant),
          year=factor(year(date)),
          notes = notes %>% fct_explicit_na("blank"),
          flowering = flowering %>% as.character %>% fct_explicit_na("blank"),
          n_rosettes = as.integer(as.character(rosettes)),
          rosettes = recode(as.character(rosettes), ID = "indist", `0` = "zero" , `NULL`="blank",.default="one_or_more", ),
          status = recode(paste(notes, rosettes, flowering), !!!translate_nrf_20, .default="recheck") %>% fct_relevel(status_priority),
-         id = na_if(id, "-"),
          plotid = paste0(plot, subplot)) %>% 
-  drop_na(id) %>% 
   left_join(treatments) %>%
   mutate_if(is.character, as.factor)
 
-#TODO add 2018 censuses to cen
-cen <- full_join(cen20, rename_all(cen19, paste0, "_19"), by=c("id"="id_19")) 
+# duplicates --------------------------------------------------------------
+# When there is more than one record for a plant tag in the census, which duplicate should be kept? 
+#   Currently resolved by the following rules:
+#   
+# 1. sort by status (keep flowering > vegetative > recheck > dead_nf > tagnf)
+# 2. sort by date within status (if statuses are the same, keep the observation with the newest date)
+# 3. keep the first row and discard the other duplicates
+# 
+# This is automatic, but it may be better to resolve duplicates manually in some cases. The duplicates are written out to [spreadsheets](https://docs.google.com/spreadsheets/d/1vuQXBF8fCOjw-mYFLBk7Pm_iRUzSAZ8QmBCSJtt6IhA/edit#gid=367829026)
+
+# Write out the duplicates
+censuses <- c("2018","2018r2","2019","2020")
+joiners <- c("plot","subplot","plotid","plant","plantid")
+cen <- list(cen18, cen18r2, cen19, cen20) %>% 
+#  skip writing for speed
+#  walk2(.y=censuses, .f = ~ .x %>% group_by(plantid) %>% filter(n()>1) %>% arrange(plantid, status, desc(date)) %>% 
+#          write_sheet(ss=filter(datasheets, name=="Maxfield Results"), sheet=paste0(.y,"duplicates"))) %>% 
+  map(~ .x %>% arrange(plantid, status, desc(date)) %>% distinct(plantid, .keep_all = TRUE)) %>% # distinct() keeps the first row after sorting, discards the other rows
+  map2(paste0("_",str_remove(censuses, "20")), 
+       ~ .x %>% rename_with(paste0, !any_of(joiners), .y)) %>% 
+  reduce(full_join)
+                                                                                                                           
+cen.status <- cen %>% select(any_of(joiners)|starts_with("status")) %>% 
+  mutate(across(starts_with("status"), 
+                . %>% fct_explicit_na("no_record") %>% fct_expand(status_priority) %>% fct_relevel(status_priority)),
+         plantid = fct_reorder(factor(plantid), .desc=T, 
+                          paste(as.integer(status_18),as.integer(status_19),as.integer(status_20),as.integer(status_18r2))),
+         transition_1819 = paste(status_18, status_19, sep=">"),
+         transition_1920 = paste(status_19, status_20, sep=">"),
+         transition_181920 = paste(status_18, status_19, status_20, sep=">"))
+
+cen.status.long <- cen.status %>% select(!starts_with("transition")) %>% 
+  pivot_longer(starts_with("status"),names_to="census",values_to="status") %>% 
+  mutate(census = paste0("20",str_remove(census, "status_"))) %>% 
+  mutate(across(.fns=factor))
+
+cen.size.long <- cen %>% select(plantid|starts_with("n_rosettes")) %>% 
+  pivot_longer(starts_with("n_rosettes"),names_to="census",values_to="n_rosettes") %>%
+  mutate(census = paste0("20",str_remove(census, "n_rosettes_")))
 
 # phenology ---------------------------------------------------------------
 
@@ -241,6 +274,7 @@ ph18.raw <- read_sheet(filter(datasheets, name=="2020 Maxfield Phenology"), shee
          plantid = paste0(plotid, plant),
          plant=as.character(plant),
          julian = factor(yday(date)),
+         year="2018",
          open = rowSums(select(., starts_with("open")), na.rm=T),
          buds = rowSums(select(., starts_with("buds")), na.rm=T),
          eggs=eggs_total) %>% 
@@ -258,6 +292,7 @@ ph19.raw <- read_sheet(filter(datasheets, name=="2020 Maxfield Phenology"), shee
          plantid = paste0(plotid, plant),
          plant = as.character(plant),
          julian = factor(yday(date)),
+         year="2019",
          open = rowSums(select(., starts_with("open")), na.rm=T),
          buds = rowSums(select(., starts_with("buds")), na.rm=T),
          eggs = rowSums(select(., starts_with("eggs")), na.rm=T)) %>% 
@@ -275,6 +310,7 @@ ph20.raw <- read_sheet(filter(datasheets, name=="2020 Maxfield Phenology"), shee
          plantid = paste0(plotid, plant),
          plant = as.character(plant),
          julian = factor(yday(date)),
+         year="2020",
          open = rowSums(select(., starts_with("open")), na.rm=T),
          buds = rowSums(select(., starts_with("buds")), na.rm=T),
          eggs = rowSums(select(., starts_with("eggs")), na.rm=T)) %>% 
@@ -288,6 +324,8 @@ ph20 <- ph20.raw %>%
 
 ph <- bind_rows(list("2018" = ph18, "2019" = ph19, "2020" = ph20), .id="year") %>% 
   mutate(julian=as.integer(as.character(julian)))
+
+ph.plantid <- ph %>% left_join(sm.subplotyear) %>% group_by(year, water, water4, snow, snow_new, plot, plotid, plant, plantid) %>% summarize_at(c("open","height_cm", "VWC"), mean, na.rm=T) 
 
 # leaftraits --------------------------------------------------------------
 
@@ -335,11 +373,9 @@ nt <- read_sheet(filter(datasheets, name=="2020 Maxfield Floral Traits"), sheet=
   left_join(treatments) %>%
   mutate_if(is.character, as.factor)
 
-#Merge nectar and morphology, 
-#add avg soil moisture and snow melt date, and
-#pick traits for analysis
+#Merge morphology and nectar
 mt <- bind_rows(mt, nt)  %>% 
-  left_join(sm.subplotyear)
+  left_join(sm.subplotyear) #add avg soil moisture and snow melt date
 
 #average by plant and year
 mt.plantyr <- mt %>% mutate_at(c("plotid","plant"), as.character) %>% 
@@ -350,7 +386,6 @@ mt.plantyr <- mt %>% mutate_at(c("plotid","plant"), as.character) %>%
 mt.subplot <- mt.plantyr %>%  
   group_by(year, water, water4, snow, snow_new, plot, plotid) %>% summarize_if(is.numeric, mean, na.rm=T)%>% ungroup %>% 
   mutate_if(is.numeric, ~replace(., is.nan(.), NA))
-
 
 # floralvolatiles ---------------------------------------------------------------
 
@@ -374,17 +409,20 @@ sds18 <- read_sheet(filter(datasheets, name=="2020 Maxfield Seeds"), sheet="2018
          seeds_mature = seeds - seeds_early,
          fruits_mature = fruits - fruits_early_countable,
          aborts = aborted_nofruit + fruits_aborted,
-         flowers_buds = flowers_buds_not_aborted + flowers_buds_not_aborted) %>% 
+         flowers_buds = flowers_buds_collected_early + flowers_buds_collected_last) %>% 
   left_join(treatments) %>%
   mutate_if(is.character, as.factor) 
+
+counts_19_20 <- c("seeds","fruits","fruits_split","aborts","fruits_fly_no_seeds","fruits_fly_with_seeds","seeds_fly","fruits_caterpillar","fruits_early_uncountable","flowers_buds","flowers_buds_collected_early","flowers_buds_collected_last")
 
 sds19 <- read_sheet(filter(datasheets, name=="2020 Maxfield Seeds"), sheet="2019", skip=1) %>% 
   mutate(plot = as.character(plot), 
          plotid = paste0(plot, subplot),
          plantid = paste0(plotid, plant),
          plant = as.character(plant),
-         year = "2019") %>%
-  mutate(across(5:15, replace_na, 0)) %>% 
+         year = "2019",
+         flowers_buds = flowers_buds_collected_early + flowers_buds_collected_last) %>%
+  mutate(across(all_of(counts_19_20), replace_na, 0)) %>% 
   left_join(treatments) %>%
   mutate_if(is.character, as.factor) 
 
@@ -395,32 +433,15 @@ sds20.date <- read_sheet(filter(datasheets, name=="2020 Maxfield Seeds"), sheet=
          plant = as.character(plant),
          julian = factor(yday(date)),
          year = "2020") %>% 
-  mutate(across(5:14, replace_na, 0)) %>% 
+  mutate(flowers_buds_collected_last = ifelse(yday(date) >= 238, flowers_buds, 0),
+         flowers_buds_collected_early = ifelse(yday(date) < 238, flowers_buds, 0)) %>% 
+  mutate(across(counts_19_20, replace_na, 0)) %>%
   left_join(treatments) %>%
   mutate_if(is.character, as.factor) 
-counts20 <- colnames(sds20.date)[5:14]
 
-sds20 <- sds20.date %>% group_by(across(c("plant", "plantid",colnames(treatments)))) %>% 
-  summarize(across(counts20, sum, na.rm=T), dates=n(), .groups="drop")
-
-# TODO for sds20, split flowers into early and last collections
-
-sds <- bind_rows(sds18, sds19, sds20) %>% 
-  left_join(sm.subplotyear) %>% 
-  mutate(#flowers_destroyed	= flowers_morphology + flowers_nectar + flowers_color + flowers_volatiles,
-    flowers_destroyed = 0,
-    flowers_buds_collected_last = 0,
-         seeds_per_fruit	= seeds / fruits,
-         fruits_aborted	= aborts + flowers_buds_collected_last,
-         seeds_est	= seeds + seeds_fly + (flowers_buds_collected_early + flowers_destroyed + fruits_early_uncountable) * (seeds/(fruits + fruits_aborted + fruits_fly_with_seeds + fruits_fly_no_seeds + fruits_caterpillar)) + fruits_split * seeds_per_fruit, 
-         fruits_with_seeds	= fruits + fruits_split + fruits_fly_with_seeds, 
-         fruits_nonaborted	= fruits_with_seeds + fruits_fly_no_seeds + fruits_caterpillar + fruits_split,
-         flowers_est	= fruits_nonaborted + fruits_aborted + flowers_buds + flowers_destroyed,
-         prop_infested	= (fruits_fly_no_seeds + fruits_fly_with_seeds + fruits_caterpillar) / fruits_nonaborted,
-         prop_aborted	= fruits_aborted / (fruits_nonaborted + fruits_aborted),
-         prop_nonaborted	= fruits_nonaborted / (fruits_nonaborted + fruits_aborted),
-         seeds_per_flower = seeds_est / flowers_est)
-
+sds20 <- sds20.date %>% 
+  group_by(across(c("plant", "plantid", colnames(treatments)))) %>% 
+  summarize(across(all_of(counts_19_20), sum, na.rm=T), dates=n(), .groups="drop")
 
 sds20.date.zeroed <- sds20.date %>% 
   mutate(julian=recode(julian, "215"="216")) %>% 
@@ -429,9 +450,93 @@ sds20.date.zeroed <- sds20.date %>%
   mutate(julian=as.integer(as.character(julian)),
          seeds_per_fruit = seeds/fruits,
          fruits_with_seeds = fruits + fruits_split + fruits_fly_with_seeds,
-         fruits_nonaborted = fruits_with_seeds + fruits_fly_no_seeds + fruits_caterpillar,
-         prop_fly = (fruits_fly_no_seeds + fruits_fly_with_seeds) / fruits_nonaborted,
+         fruits_nonaborted = fruits_with_seeds + fruits_fly_no_seeds + fruits_caterpillar + fruits_split,
+         prop_infested	= (fruits_fly_no_seeds + fruits_fly_with_seeds + fruits_caterpillar) / fruits_nonaborted,
          fruiting = fruits_nonaborted > 0)
+
+# megatally ---------------------------------------------------------------
+
+megatally <- list(
+  cen.status,
+  cen18 %>% group_by(plotid, plant) %>% tally(name="census18"),
+  cen18r2 %>% group_by(plotid, plant) %>% tally(name="census18r2"),
+  cen19 %>% group_by(plotid, plant) %>% tally(name="census19"),
+  cen20 %>% group_by(plotid, plant) %>% tally(name="census20"),
+  lt %>% filter(year=="2018") %>% group_by(plotid, plant) %>% tally(name="leaf18"),
+  lt %>% filter(year=="2019") %>% group_by(plotid, plant) %>% tally(name="leaf19"),
+  lt %>% filter(year=="2020") %>% group_by(plotid, plant) %>% tally(name="leaf20"),
+  mt %>% filter(year=="2018") %>% group_by(plotid, plant) %>% tally(name="floral18"),
+  mt %>% filter(year=="2019") %>% group_by(plotid, plant) %>% tally(name="floral19"),
+  mt %>% filter(year=="2020") %>% group_by(plotid, plant) %>% tally(name="floral20"),
+  vt %>% filter(year=="2020") %>% group_by(plotid, plant) %>% tally(name="volatiles20"),
+  ph18.raw %>% group_by(plotid, plant) %>% tally(name="pheno18"),
+  ph19.raw %>% group_by(plotid, plant) %>% tally(name="pheno19"),
+  ph20.raw %>% group_by(plotid, plant) %>% tally(name="pheno20"),
+  sds18 %>% group_by(plotid, plant) %>% tally(name="seeds18"),
+  sds19 %>% group_by(plotid, plant) %>% tally(name="seeds19"),
+  sds20 %>% group_by(plotid, plant) %>% tally(name="seeds20")
+) %>% 
+  reduce(full_join) %>% arrange(plotid, plant) %>% 
+  mutate(across(where(is.integer), replace_na, 0),
+         plantid = factor(paste0(plotid,plant)),
+         plot = factor(str_sub(plotid,1,1)), subplot = factor(str_sub(plotid,2,2))) %>% 
+  rowwise() %>%  mutate(total_obs = sum(c_across(census18:seeds20), na.rm=T)) %>% ungroup
+
+megatally %>% 
+  write_csv("data/megatally.csv")
+  #write_sheet(ss=filter(datasheets, name=="Maxfield Results"), sheet="megatally")
+
+megatally.status <- megatally %>% select(!starts_with("census")) %>% 
+  mutate(across(starts_with(c("volatiles","seeds")), recode, `0`="no_record", .default="flowering"),
+         across(starts_with(c("pheno","floral")), recode, `0`="no_record", `1` = "recheck", .default="flowering"),
+         across(starts_with("leaf"), recode, `0`="no_record", .default="vegetative"),
+         across(starts_with(c("pheno","floral","volatiles","seeds","leaf")), fct_expand, status_priority),
+         across(where(is.factor), fct_explicit_na, "no_record")) %>% 
+  arrange(status_18, pheno18, seeds18, status_19, pheno19, seeds19, status_20, pheno20, seeds20, leaf18, leaf19, leaf20, status_18r2, total_obs) %>% 
+  mutate(plantid = fct_reorder(plantid, row_number(), .desc=T))
+
+megatally.status.long <- megatally.status %>% select(!starts_with(c("transition","total_obs"))) %>% 
+  pivot_longer(status_18:seeds20,names_to="dataset",values_to="status") %>% 
+  mutate(across(.fns=factor),
+         dataset = fct_relevel(dataset, c("status_18","status_18r2","status_19","status_20","pheno18","seeds18","floral18","pheno19","seeds19","floral19","pheno20","seeds20","floral20","volatiles20","leaf18","leaf19","leaf20")))
+
+
+sds <- bind_rows(sds18, sds19, sds20) %>%
+  left_join(sm.subplotyear) %>% 
+  mutate(plantid = paste0(plotid,plant)) %>% 
+  left_join(megatally %>% select(plantid|starts_with("floral")) %>% 
+              pivot_longer(starts_with("floral"),names_to="year",values_to="flowers_morphology_nectar") %>% 
+              mutate(year = paste0("20",str_remove(year,"floral")))) %>% 
+  left_join(megatally %>% select(plantid|starts_with("volatiles")) %>% 
+              pivot_longer(starts_with("volatiles"),names_to="year",values_to="flowers_volatiles") %>% 
+              mutate(year = paste0("20",str_remove(year,"volatiles")))) %>% 
+  mutate(
+    flowers_volatiles = replace_na(flowers_volatiles, 0), #TODO get volatiles for 2018 & 2019
+    flowers_destroyed = flowers_morphology_nectar + flowers_volatiles, #TODO + flowers_color
+    seeds_per_fruit	= seeds / fruits,
+    fruits_aborted	= aborts + flowers_buds_collected_last,
+    seeds_est	= seeds + seeds_fly + (flowers_buds_collected_early + flowers_destroyed + fruits_early_uncountable) * (seeds/(fruits + fruits_aborted + fruits_fly_with_seeds + fruits_fly_no_seeds + fruits_caterpillar)) + fruits_split * seeds_per_fruit, 
+    fruits_with_seeds	= fruits + fruits_split + fruits_fly_with_seeds, 
+    fruits_nonaborted	= fruits_with_seeds + fruits_fly_no_seeds + fruits_caterpillar + fruits_split,
+    flowers_est	= fruits_nonaborted + fruits_aborted + flowers_buds + flowers_destroyed,
+    prop_infested	= (fruits_fly_no_seeds + fruits_fly_with_seeds + fruits_caterpillar) / fruits_nonaborted,
+    prop_aborted	= fruits_aborted / (fruits_nonaborted + fruits_aborted),
+    prop_nonaborted	= fruits_nonaborted / (fruits_nonaborted + fruits_aborted),
+    seeds_per_flower = seeds_est / flowers_est)
+
+######## Merge morphology, nectar, phenology, and seeds ####
+mnps <- bind_rows(mt, ph, sds)
+
+#average by plant and year
+mnps.plantyr <- mnps %>% mutate_at(c("plotid","plant"), as.character) %>% 
+  group_by(year, water, water4, snow, snow_new, plot, plotid, plant) %>% summarize_if(is.numeric, mean, na.rm=T) %>% ungroup %>% 
+  mutate_if(is.numeric, ~replace(., is.nan(.), NA))
+
+#average by plant and year, then by subplot
+mnps.subplot <- mnps.plantyr %>%  
+  group_by(year, water, water4, snow, snow_new, plot, plotid) %>% summarize_if(is.numeric, mean, na.rm=T)%>% ungroup %>% 
+  mutate_if(is.numeric, ~replace(., is.nan(.), NA))
+
 # export ------------------------------------------------------------------
 
 remove(wx)
