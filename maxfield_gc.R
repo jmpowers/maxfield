1ca3cd7f23bd8389c959f4e80384c1d4fae5fa43
jmpowers <icyjava@gmail.com>
2023-09-18 16:17:16 -0700

    generate maxfield_all from this project's markes_sequence.rda

--- maxfield_gc.R
+++ maxfield_gc.R
@@ -1,18 +1,13 @@
 library(tidyverse)
 library(reshape2)
-library(lubridate)
 library(vegan)
 library(ggvegan)
-library(googlesheets4)
-gs4_auth(email = T)
-
-setwd("~/MyDocs/MEGA/UCI/Schiedea/Analysis/scent/rmbl/Maxfield")
-proj_dir <- "~/MyDocs/MEGA/UCI/RMBL 2020/maxfield/"
-source("../read_shimadzu.R")
 
 # Read chromatograms ------------------------------------------------------
 
 #reading in the Shimadzu search output is slow, skip it
+# setwd("~/MyDocs/MEGA/UCI/Schiedea/Analysis/scent/rmbl/Maxfield")
+# source("../read_shimadzu.R")
 # setwd("./data")
 # datafiles <- list.files(pattern=".txt")
 # #safemadzu <- safely(read.shimadzu)
@@ -20,11 +15,14 @@ source("../read_shimadzu.R")
 # #map(shimadzu.data, "error") %>% map_lgl(~!is.null(.)) %>% sum #tally import errors
 # maxf.data <-  datafiles %>% set_names() %>% map(read.shimadzu) %>% bind_rows(.id="batch")
 # setwd("..")
-# #save(maxf.data, file="maxfield210827.Rdata")
+# #save(maxf.data, file="maxfield210827.Rdata") #copied this to maxfield folder:
 
-load("maxfield210827.Rdata")
-maxf.data <- maxf.data %>% filter(batch != "ipo2015.2016.txt")
-sort(unique(maxf.data$batch))
+load("data/volatiles/maxfield210827.Rdata")
+maxf.data <- maxf.data %>% filter(!batch %in% c("ipo2015.2016.txt"))#, #old files
+                                  #"heather_maxfield190813_new.txt")) #TODO filter these dupes out later - subset of heather_maxfield190813_new001.txt
+# Duplicate filenames in Shimadzu output:
+# 2 unique chomatograms for Blank1_762021_01.qgd
+#maxf.data %>% count(Filename,batch) %>% group_by(Filename) %>% filter(n()>1)
 
 maxf.all <- dcast(maxf.data, Filename~Name, sum, value.var="Area")
 rownames(maxf.all) <- maxf.all[,1]
@@ -35,7 +33,6 @@ maxf.cut <- maxf.all[,colSums(maxf.all)>5e8]#arbitrary cutoff
 k <- 40
 set.seed(1)
 km <- kmeans(decostand(maxf.cut, method="log"), k, nstart=3)
-#save(km, file="km30.Rdata")
 
 maxf.km <- tibble(FileName=rownames(maxf.all)) %>% 
   mutate(rowSum = rowSums(maxf.all),
@@ -45,16 +42,15 @@ maxf.km <- tibble(FileName=rownames(maxf.all)) %>%
          runYear = str_extract(FileName, "2018|2019|2020|2021") %>% replace_na("2018") %>% factor,
          Cluster = km$cluster) %>% # Figure out which k-means clusters are the blanks
   mutate(kBlank = Cluster %in% (count(., nameBlank, Cluster) %>% filter(nameBlank, n>2) %>% pull(Cluster)),
-         Mixup = nameBlank != kBlank)
+         Mixup = nameBlank != kBlank) #mixup if filename=Blank but doesn't cluster with blanks, or vice versa
 
 with(maxf.km, table(kBlank, nameBlank))
 
 #  Filter Maxfield samples from inventory -----------------------------------------------------------
 # Moved this "Maxfield runs" section from markes_sequence.R, code above that section outputs this Rdata:
-#TODO the new markes_sequence is likely causing the discrepancies with 210827_updated - 2022 data processed with different fuzzy join parameters. original +0 offset, +-16 min tolerance, now +18 +-15
-#there is old markes_sequence.R and an old markes_sequence.rda copied over to the maxfield project that might work
-
-load("../Inventory/markes_sequence.rda")
+#fixed: the new "../Inventory/markes_sequence.rda" is likely causing the discrepancies with 210827_updated - 2022 data processed with different fuzzy join parameters. original +0 offset, +-16 min tolerance, now +18 +-15
+#trying the old markes_sequence.rda (2021-12-02), also see maxfield/markes_sequence.R (same date)
+load("data/volatiles/markes_sequence.rda") #loads sequ, sequ.file, sequ.summary, qgdfiles
 
 #Try to get all 2021 data to annotate, not just maxfield samples
 #811-8132021 are for picking out 2021 Lupinus argenteus samples
@@ -63,9 +59,10 @@ Maxfield <- str_detect(sequ.summary$FullName, "Maxfield|07262018|June28_07292018
 maxfield.batchids <- sequ.summary %>% filter(Maxfield) %>% select(id) %>% unique() %>% na.omit() 
 maxfgc <- sequ.summary %>% filter(id %in% maxfield.batchids$id | Maxfield) #get entire batch if it had a sample that matches
 
-#check for duplicate filenames in Markes/GC sequence and Shimadzu text files
-count(maxfgc, FileName) %>% arrange(desc(n)) #187 NAs, 2x Blank1_762021_01.qgd, 2x Corydalis_200727_12_redo_762021_08.qgd
-maxf.data %>% group_by(Filename,batch) %>% tally() %>% count(Filename) %>% arrange(desc(n)) #six 190805_8112019 files duplicated
+# Duplicate filenames in Markes/GC sequence: 
+#   2x2 unique chromatograms for both Blank1_070621_1.qgd, Corydalis_200727_12_redo_762021_08.qgd (OK)
+#   8x2 mismatch fuzzy matchings with one file, two close desorb times (will remove with verdict=mismatch)
+maxfgc %>% drop_na(FileName) %>% group_by(FileName) %>% filter(n()>1) %>% arrange(FileName)
 
 #join Markes/GC sequence, k-means, and batch file names
 maxfgc <- maxfgc %>% left_join(maxf.km %>% select(FileName, nameBlank, Mixup, kBlank, Cluster)) %>% 
@@ -74,21 +71,23 @@ maxfgc <- maxfgc %>% left_join(maxf.km %>% select(FileName, nameBlank, Mixup, kB
   select(c("index", "sequence.start", "batch", "Desorb.Start.Time", "CreationTime", "eithertime", "status", 
            "Tube", "markes_n", "GC_n", "either_n", "markes_GC", "create_desorb", "desorb.Start.diff", 
            "Mixup", "nameBlank", "kBlank", "Cluster", "n_peaks", "verdict", "FileName", "sample", "user", "FullName", "id"))
-write_csv(maxfgc, "maxfield_all210827_updated.csv")#in the rmbl/Maxfield folder! differs from project directory
+write_csv(maxfgc, "data/volatiles/maxfield_all210827_updated.csv")#used to output to the rmbl/Maxfield folder. Original from project folder backed up in RMBL 2020
 
 #Output only Maxfield samples to split filenames into parts
-data_inventory <- "1X8oo7qZlo1p6MVl_CBeBe6CUTHEAcd-FWQzfHud3Qws" #"RMBL GC-MS Data Inventory"
-read_sheet(data_inventory, sheet="maxfield_all210827annot", na="NA") %>% 
-  filter(user=="D Campbell : Maxfield") %>% 
-  mutate(sample = na_if(sample,"") %>% coalesce(FileName) %>% str_remove(".qgd")) %>% #renames skips
-  select(index, batch, Desorb.Start.Time, verdict, sample) %>% 
-  separate(sample, into=paste0("file",1:9), remove=F) #%>% 
-#range_write(ss=data_inventory, sheet="maxfield_meta",range="A:N") #output to sheet - do not uncomment!
+# library(googlesheets4)
+# gs4_auth(email = T)
+# data_inventory <- "1X8oo7qZlo1p6MVl_CBeBe6CUTHEAcd-FWQzfHud3Qws" #"RMBL GC-MS Data Inventory"
+# read_sheet(data_inventory, sheet="maxfield_all210827annot", na="NA") %>% 
+#   filter(user=="D Campbell : Maxfield") %>% 
+#   mutate(sample = na_if(sample,"") %>% coalesce(FileName) %>% str_remove(".qgd")) %>% #renames skips
+#   select(index, batch, Desorb.Start.Time, verdict, sample) %>% 
+#   separate(sample, into=paste0("file",1:9), remove=F) #%>% 
+# #range_write(ss=data_inventory, sheet="maxfield_meta",range="A:N") #output to sheet - do not uncomment!
 
 #get hand-split filename metadata
 #some of these only have vial numbers, but it doesn't matter since they were sampled in 2021 OTC experiment
 #index is from maxfield_all210827annot, sample accounts for skips
-#TODO maxfgc and maxfield_meta gsheet index columns do not line up so this join is broken - 
+#FIXED maxfgc and maxfield_meta gsheet index columns do not line up so this join is broken - 
 # looks like it broke with the maxfield_all210827_updated.csv (versus maxfield_all210827.csv)
 maxfmeta <- read_sheet(data_inventory, sheet="maxfield_meta", guess_max=2000, col_types="c") %>% 
   select(index, sample, type:vial) %>% as.data.frame %>% #added sample (renamed)
@@ -99,24 +98,26 @@ maxfmeta <- read_sheet(data_inventory, sheet="maxfield_meta", guess_max=2000, co
          plant= ifelse(type=="floral", str_sub(plantid, 3),NA),
          sampledate=ymd(ifelse(year(sequence.start)==2018, paste0("2018",sampledate), sampledate)))
 
-#TODO testing differences between these versions:
-maxfgcnew <- read_csv("maxfield_all210827_updated.csv") #different from the other two
-maxfgcmid <- read_csv(paste0(proj_dir,"data/volatiles/maxfield_all210827_updated.csv"))#almost identical to old
-maxfgcold <- read_csv("maxfield_all210827.csv")
-plot(maxfgcmid$Cluster,maxfgcold$Cluster, pch=19, col=alpha("black",0.1))#kmeans clusters are difference though
-plot(match(maxfgcnew$Desorb.Start.Time, maxfgcold$Desorb.Start.Time))
-plot(match(maxfgcold$Desorb.Start.Time, maxfgcnew$Desorb.Start.Time))
-plot(maxfgcnew$Desorb.Start.Time)
-plot(maxfgcold$Desorb.Start.Time)
-setdiff(maxfgcold$FileName, maxfgcnew$FileName) #some old filenames deleted - 2018 and 2019
-setdiff(maxfgcnew$FileName, maxfgcold$FileName) #no new filenames
-maxfgcold$deleted <- maxfgcold$FileName %in% setdiff(maxfgcold$FileName, maxfgcnew$FileName) 
-plot(maxfgcold$deleted)
-oldnew <- full_join(maxfgcold, 
-                    maxfgcnew %>% rename(newindex=index) %>% 
-                      select(-CreationTime, -eithertime, -create_desorb, -Mixup, -kBlank, -Cluster))
-sum(is.na(oldnew$newindex))/nrow(oldnew)
-plot(oldnew$index, oldnew$newindex)
+# #testing differences between these versions, decided that oldseq will work
+# origpath <- "~/MyDocs/MEGA/UCI/Schiedea/Analysis/scent/rmbl/Maxfield/"
+# maxfgcnew <- read_csv(paste0(origpath, "maxfield_all210827_updated.csv"))# different from the other two
+# maxfgcmid <- read_csv("data/volatiles/maxfield_all210827_updated.csv")#almost identical to old
+# maxfgcold <- read_csv(paste0(origpath, "maxfield_all210827.csv"))
+# plot(maxfgcmid$Cluster,maxfgcold$Cluster, pch=19, col=alpha("black",0.1))#kmeans clusters are different though
+# plot(match(maxfgcnew$Desorb.Start.Time, maxfgcold$Desorb.Start.Time))
+# plot(match(maxfgcold$Desorb.Start.Time, maxfgcnew$Desorb.Start.Time))
+# plot(maxfgcnew$Desorb.Start.Time)
+# plot(maxfgcold$Desorb.Start.Time)
+# setdiff(maxfgcold$FileName, maxfgcnew$FileName) #some old filenames deleted - 2018 and 2019
+# setdiff(maxfgcnew$FileName, maxfgcold$FileName) #no new filenames
+# maxfgcold$deleted <- maxfgcold$FileName %in% setdiff(maxfgcold$FileName, maxfgcnew$FileName) 
+# plot(maxfgcold$deleted)
+# oldnew <- full_join(maxfgcold, 
+#                     maxfgcnew %>% rename(newindex=index) %>% 
+#                       select(-CreationTime, -eithertime, -create_desorb, -Mixup, -kBlank, -Cluster))
+# sum(is.na(oldnew$newindex))/nrow(oldnew)
+# plot(oldnew$index, oldnew$newindex)
+# maxfgcoldseq <- read_csv("data/volatiles/maxfield_all210827_oldseq.csv")#newly created from maxfield/data/volatiles/markes_sequence.rda (2021-12-02), identical to mid except for quotes around "NA NA"
 
 #subset wide data to only Maxfield samples with an annotated type (floral/ambient) from maxfmeta
 maxf <- maxf.all[maxfmeta$FileName,]
