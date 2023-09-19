82dba343516206c7ab44b77f75952df747c3a28d
jmpowers <icyjava@gmail.com>
2023-09-18 22:19:51 -0700

    remove double batch dupes

--- maxfield_gc.R
+++ maxfield_gc.R
@@ -16,11 +16,12 @@ library(reshape2)
 # #save(maxf.data, file="maxfield210827.Rdata") #copied this to maxfield folder:
 
 load("data/volatiles/maxfield210827.Rdata")
-maxf.data <- maxf.data %>% filter(!batch %in% c("ipo2015.2016.txt"))#, #old files
-                                  #"heather_maxfield190813_new.txt")) #TODO filter these dupes out later - subset of heather_maxfield190813_new001.txt
+maxf.data <- maxf.data %>% filter(!batch %in% c("ipo2015.2016.txt", #old files
+                                  "heather_maxfield190813_new.txt")) #subset of heather_maxfield190813_new001.txt
 # Duplicate filenames in Shimadzu output:
 # 2 unique chomatograms for Blank1_762021_01.qgd
-#maxf.data %>% count(Filename,batch) %>% group_by(Filename) %>% filter(n()>1)
+#These two will get added together in maxf.all
+maxf.data %>% count(Filename,batch) %>% group_by(Filename) %>% filter(n()>1)
 
 maxf.all <- dcast(maxf.data, Filename~Name, sum, value.var="Area")
 rownames(maxf.all) <- maxf.all[,1]
@@ -48,6 +49,7 @@ with(maxf.km, table(kBlank, nameBlank))
 # Moved this "Maxfield runs" section from markes_sequence.R, code above that section outputs this Rdata:
 #fixed: the new "../Inventory/markes_sequence.rda" is likely causing the discrepancies with 210827_updated - 2022 data processed with different fuzzy join parameters. original +0 offset, +-16 min tolerance, now +18 +-15
 #trying the old markes_sequence.rda (2021-12-02), also see maxfield/markes_sequence.R (same date)
+#TODO make sure this old version can be generated with that old code
 load("data/volatiles/markes_sequence.rda") #loads sequ, sequ.file, sequ.summary, qgdfiles
 
 #Try to get all 2021 data to annotate, not just maxfield samples
@@ -55,21 +57,23 @@ load("data/volatiles/markes_sequence.rda") #loads sequ, sequ.file, sequ.summary,
 #There are some 2021 GNA, VF runs in Test instrument runs too, but adding that folder would break pre-2021 verdict order
 Maxfield <- str_detect(sequ.summary$FullName, "Maxfield|07262018|June28_07292018|Ipomopsis 2020|Corydalis|8112021|8122021|8132021")
 maxfield.batchids <- sequ.summary %>% filter(Maxfield) %>% select(id) %>% unique() %>% na.omit() 
-maxfgc <- sequ.summary %>% filter(id %in% maxfield.batchids$id | Maxfield) #get entire batch if it had a sample that matches
+maxfgc.sequ <- sequ.summary %>% filter(id %in% maxfield.batchids$id | Maxfield) #get entire batch if it had a sample that matches
 
-# Duplicate filenames in Markes/GC sequence: 
-#   2x2 unique chromatograms for both Blank1_070621_1.qgd, Corydalis_200727_12_redo_762021_08.qgd (OK)
-#   8x2 mismatch fuzzy matchings with one file, two close desorb times (will remove with verdict=mismatch)
-maxfgc %>% drop_na(FileName) %>% group_by(FileName) %>% filter(n()>1) %>% arrange(FileName)
+# TODO Duplicate filenames in Markes/GC sequence: 
+#   2x2 unique chromatograms for both Blank1_070621_1.qgd, Corydalis_200727_12_redo_762021_08.qgd (OK) - gives join warning message below
+#   8x2 mismatch fuzzy matchings with one file, two close desorb times (can remove later with verdict=mismatch?)
+maxfgc.sequ %>% drop_na(FileName) %>% group_by(FileName) %>% filter(n()>1) %>% arrange(FileName)
 
 #join Markes/GC sequence, k-means, and batch file names
-maxfgc <- maxfgc %>% left_join(maxf.km %>% select(FileName, nameBlank, Mixup, kBlank, Cluster)) %>% 
+#many-tomany warning is from duplicate filenames in maxf.data (2 blanks), and duplicate filenames in Markes/GC sequence (mismatches)
+maxfgc <- maxfgc.sequ %>% 
+  left_join(maxf.km %>% select(FileName, nameBlank, Mixup, kBlank, Cluster)) %>% 
   mutate(verdict="", sample="", index=row_number()) %>% 
   left_join(maxf.data %>% rename(FileName=Filename) %>% group_by(FileName,batch) %>% tally(name="n_peaks")) %>% #get batch file names from Shimadzu output
   select(c("index", "sequence.start", "batch", "Desorb.Start.Time", "CreationTime", "eithertime", "status", 
            "Tube", "markes_n", "GC_n", "either_n", "markes_GC", "create_desorb", "desorb.Start.diff", 
-           "Mixup", "nameBlank", "kBlank", "Cluster", "n_peaks", "verdict", "FileName", "sample", "user", "FullName", "id"))
-write_csv(maxfgc, "data/volatiles/maxfield_all210827_updated.csv")#used to output to the rmbl/Maxfield folder. Original from project folder backed up in RMBL 2020
+           "Mixup", "nameBlank", "kBlank", "Cluster", "n_peaks", "verdict", "FileName", "sample", "user", "FullName", "id")) %>% 
+  write_csv("data/volatiles/maxfield_all210827_lessdupes.csv")#used to output to the rmbl/Maxfield folder. Original from project folder backed up in RMBL 2020/bak
 
 #Output only Maxfield samples to split filenames into parts
 # library(googlesheets4)
@@ -90,14 +94,22 @@ write_csv(maxfgc, "data/volatiles/maxfield_all210827_updated.csv")#used to outpu
 # looks like it broke with the maxfield_all210827_updated.csv (versus maxfield_all210827.csv)
 #read_sheet(data_inventory, sheet="maxfield_meta", guess_max=2000, col_types="c") %>% 
 maxfmeta <- read_tsv("data/volatiles/RMBL GC-MS Data Inventory - maxfield_meta.tsv") %>% 
+  filter(batch!="heather_maxfield190813_new.txt") %>% #drop duplicate batch
   select(index, sample, type:vial) %>% as.data.frame %>% #added sample (renamed)
   drop_na(type) %>% # exclude files that don't have a manual "type" entry - blanks, leaks, skips, OTC experiment
-  distinct(index, .keep_all = T) %>% # TODO investigate these dupes with two Shimadzu batches
-  left_join(maxfgc %>% distinct(index, .keep_all = T) %>% select(-sample)) %>% #took off sample (empty)
+  #distinct(index, .keep_all = T) %>% # FIXED dupes with two Shimadzu batches
+  left_join(maxfgc %>% distinct(index, .keep_all = T) %>% select(-sample)) %>% #took off sample (empty) 
   mutate(plotid=ifelse(type=="floral", str_sub(plantid, 1,2),NA),
          plant= ifelse(type=="floral", str_sub(plantid, 3),NA),
          sampledate=ymd(ifelse(year(sequence.start)==2018, paste0("2018",sampledate), sampledate)),
-         index=as.character(index))
+         index=as.character(index)) %>% 
+  write_csv("data/volatiles/maxfmeta_lessdupes.csv")
+
+# #testing to see if duplicates can be removed without breaking index orders of maxfgc and maxfield_meta
+# read_tsv("data/volatiles/RMBL GC-MS Data Inventory - maxfield_meta.tsv") %>% filter(index %in% index[duplicated(index)]) %>% with(table(sample, batch))
+# read_tsv("data/volatiles/RMBL GC-MS Data Inventory - maxfield_meta.tsv") %>% filter(batch=="heather_maxfield190813_new.txt")
+# maxfgc %>% filter(index %in% index[duplicated(index)])
+# maxfgc %>% filter(batch=="heather_maxfield190813_new.txt")
 
 # #testing differences between these versions, decided that oldseq will work
 # origpath <- "~/MyDocs/MEGA/UCI/Schiedea/Analysis/scent/rmbl/Maxfield/"
@@ -123,7 +135,7 @@ maxfmeta <- read_tsv("data/volatiles/RMBL GC-MS Data Inventory - maxfield_meta.t
 #subset wide data to only Maxfield samples with an annotated type (floral/ambient) from maxfmeta
 maxf <- maxf.all[maxfmeta$FileName,]
 rownames(maxf) <- rownames(maxfmeta) <- maxfmeta$index
-save(maxf, maxfmeta, file="data/volatiles/maxfield_volatiles.rda")
+save(maxf, maxfmeta, file="data/volatiles/maxfield_volatiles_lessdupes.rda")
 
 # # NMDS of blanks and all samples --------------------------------------------------------------------
 # ggplot(maxfgc, aes(x=n_peaks, fill=paste(nameBlank,kBlank))) + geom_histogram() + facet_wrap(vars(year(eithertime)))
